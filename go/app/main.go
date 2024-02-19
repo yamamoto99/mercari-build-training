134c4f0b883d746b0b89d8a1f3eaee56339e9c6d
Masato Yamamoto <masato.yamamoto99@gmail.com>
2024-02-19 12:18:08 +0900

    feat:step4-1done

--- go/app/main.go
+++ go/app/main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"crypto/sha256"
+	"database/sql"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/log"
+	_ "github.com/mattn/go-sqlite3"
 	"io"
 	"mime/multipart"
 	"net/http"
@@ -13,10 +18,6 @@ import (
 	"path/filepath"
 	"strconv"
 	"strings"
-
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
-	"github.com/labstack/gommon/log"
 )
 
 const (
@@ -32,6 +33,7 @@ type Items struct {
 }
 
 type Item struct {
+	Id        int    `json:"id"`
 	Name      string `json:"name"`
 	Category  string `json:"category"`
 	ImageName string `json:"imageName"`
@@ -74,6 +76,15 @@ func root(c echo.Context) error {
 	return c.JSON(http.StatusOK, res)
 }
 
+func connectDB(c echo.Context) *sql.DB {
+	// dbOpen
+	db, err := sql.Open("sqlite3", "../db/mercari.sqlite3")
+	if err != nil {
+		c.Logger().Fatalf("DB接続エラー: %v", err)
+	}
+	return db
+}
+
 func addItem(c echo.Context) error {
 	// Get form data
 	name := c.FormValue("name")
@@ -82,63 +93,106 @@ func addItem(c echo.Context) error {
 	if err != nil {
 		c.Logger().Fatalf("画像の受け取りに失敗しました: %v", err)
 	}
+	c.Logger().Debugf("Receive item: %s", name)
+	c.Logger().Debugf("Receive category: %s", category)
+	c.Logger().Debugf("Receive imageFile: %s", img.Filename)
 	// 入手したファイルから名前をhash化したファイルを生成
 	newFileName, err := copyfile(img)
 	if err != nil {
 		c.Logger().Fatalf("%v", err)
 	}
+	db := connectDB(c)
+	// close処理
+	defer func(db *sql.DB) {
+		_ = db.Close()
+	}(db)
+	// ステートメントを生成
+	stmt, err := db.Prepare("INSERT INTO items (name, category, image_name) VALUES (?, ?, ?)")
+	if err != nil {
+		c.Logger().Fatalf("SQL書き込みエラー: %v", err)
+	}
+	// stmtClose
+	defer func(data *sql.Stmt) {
+		_ = data.Close()
+	}(stmt)
+	// ステートメントを用いて書き込み
+	if _, err = stmt.Exec(name, category, newFileName); err != nil {
+		c.Logger().Fatalf("SQL書き込みエラー: %v", err)
+	}
 
-	c.Logger().Infof("Receive item: %s", name)
-	c.Logger().Infof("Receive category: %s", category)
-	c.Logger().Infof("Receive imageFile: %s", newFileName)
+	// ▽JSONに保存▽
 
-	item := Item{Name: name, Category: category, ImageName: newFileName}
-	// jsonファイル読み込み
-	inJsonItems, err := os.ReadFile("items.json")
-	if err != nil {
-		c.Logger().Fatalf("JSONデータを読み込めませんでした: %v", err)
-	}
-	var inItems Items
-	// 読み込んだファイルが空ではない時JSONを構造体に変換
-	if len(inJsonItems) != 0 {
-		err = json.Unmarshal(inJsonItems, &inItems)
-		if err != nil {
-			c.Logger().Fatalf("JSONファイルを構造体に変換できませんでした: %v", err)
-		}
-	}
-	// 構造体にformの値を追加
-	inItems.Items = append(inItems.Items, item)
-	// json.MarshalでJSON形式に変換
-	output, err := json.Marshal(&inItems)
-	if err != nil {
-		c.Logger().Fatalf("JSON生成中にエラーが発生しました: %v", err)
-	}
-	// jsonファイルを作成、すでにある場合はクリアして開く
-	file, err := os.Create("items.json")
-	if err != nil {
-		c.Logger().Fatalf("JSONファイルを開けませんでした: %v", err)
-	}
-	// ファイルclose
-	defer func(file *os.File) {
-		_ = file.Close()
-	}(file)
-	// jsonファイル書き込み
-	if _, err = file.Write(output); err != nil {
-		c.Logger().Fatalf("JSONファイル書き込み中にエラーが発生しました: %v", err)
-	}
+	//item := Item{Name: name, Category: category, ImageName: newFileName}
+	//// jsonファイル読み込み
+	//inJsonItems, err := os.ReadFile("items.json")
+	//if err != nil {
+	//	c.Logger().Fatalf("JSONデータを読み込めませんでした: %v", err)
+	//}
+	//var inItems Items
+	//// 読み込んだファイルが空ではない時JSONを構造体に変換
+	//if len(inJsonItems) != 0 {
+	//	err = json.Unmarshal(inJsonItems, &inItems)
+	//	if err != nil {
+	//		c.Logger().Fatalf("JSONファイルを構造体に変換できませんでした: %v", err)
+	//	}
+	//}
+	//// 構造体にformの値を追加
+	//inItems.Items = append(inItems.Items, item)
+	//// json.MarshalでJSON形式に変換
+	//output, err := json.Marshal(&inItems)
+	//if err != nil {
+	//	c.Logger().Fatalf("JSON生成中にエラーが発生しました: %v", err)
+	//}
+	//// jsonファイルを作成、すでにある場合はクリアして開く
+	//file, err := os.Create("items.json")
+	//if err != nil {
+	//	c.Logger().Fatalf("JSONファイルを開けませんでした: %v", err)
+	//}
+	//// ファイルclose
+	//defer func(file *os.File) {
+	//	_ = file.Close()
+	//}(file)
+	//// jsonファイル書き込み
+	//if _, err = file.Write(output); err != nil {
+	//	c.Logger().Fatalf("JSONファイル書き込み中にエラーが発生しました: %v", err)
+	//}
 	res := Response{Message: "書き込みが正常に完了しました。"}
 
 	return c.JSON(http.StatusOK, res)
 }
 
 func getItems(c echo.Context) error {
-	// jsonファイル読み込み
-	afterItems, err := os.ReadFile("items.json")
+	db := connectDB(c)
+	// close処理
+	defer func(db *sql.DB) {
+		_ = db.Close()
+	}(db)
+	rows, err := db.Query("SELECT id, name, category, image_name FROM items")
 	if err != nil {
-		c.Logger().Fatalf("JSONデータを読み込めませんでした: %v", err)
+		c.Logger().Fatalf("DBから値を取得できませんでした: %v", err)
 	}
-	// terminal上で末尾改行してない時に自動改行の%が付与されるのを防ぐため、あらかじめ改行を付与
-	res := append(afterItems, byte('\n'))
+	items := new(Items)
+	for rows.Next() {
+		item := Item{}
+		if err := rows.Scan(&item.Id, &item.Name, &item.Category, &item.ImageName); err != nil {
+			c.Logger().Fatalf("DBの値を変換できませんでした: %v", err)
+		}
+		items.Items = append(items.Items, item)
+	}
+	// json.MarshalでJSON形式に変換
+	output, err := json.Marshal(&items)
+	if err != nil {
+		c.Logger().Fatalf("JSON生成中にエラーが発生しました: %v", err)
+	}
+	// ▽JSONから取得▽
+
+	//// jsonファイル読み込み
+	//afterItems, err := os.ReadFile("items.json")
+	//if err != nil {
+	//	c.Logger().Fatalf("JSONデータを読み込めませんでした: %v", err)
+	//}
+	//// terminal上で末尾改行してない時に自動改行の%が付与されるのを防ぐため、あらかじめ改行を付与
+	res := append(output, byte('\n'))
 
 	// 取得したのがbyte形式だったのでJSON->JSONBlobに変更
 	return c.JSONBlob(http.StatusOK, res)
